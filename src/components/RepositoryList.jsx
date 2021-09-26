67fbb852f932e1e553637d7a3f2438d363452bfa
Renan <renan.sergipe68@gmail.com>
2021-09-26 17:41:25 -0300

    Primeiro componente

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList(){
+  return(
+    <section>
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <li>
+          <strong>Nome</strong >
+          <p>descrição</p>
+          <a href="">link</a>
+        </li>
+      </ul>
+    </section>
+  )
+}

f419213ce17946bea5ef325d35b1a7f9f3948d68
Renan <renan.sergipe68@gmail.com>
2021-09-26 18:08:35 -0300

    Propriedades no react

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,11 @@
+import { RepositoryItem } from "./RepositoryItem"
+const repository = { name: 'Exemplo', description: 'descrição', link: 'https://github.com/Renan1fps'}
 export function RepositoryList(){
   return(
     <section>
       <h1>Lista de repositórios</h1>
       <ul>
-        <li>
-          <strong>Nome</strong >
-          <p>descrição</p>
-          <a href="">link</a>
-        </li>
+        <RepositoryItem repository={repository}/>
       </ul>
     </section>
   )

173534a5e95eda69035df9c8b562445aebe0947c
Renan <renan.sergipe68@gmail.com>
2021-09-26 16:50:35 -0300

    Configurando webpack-dev-server para produtividade

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  devServer:{
+    static: './'
+  },
   plugins:[
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')

006f0bc2a8ab6d23ed5e215f78b30c5f5d6ab4e0
Renan <renan.sergipe68@gmail.com>
2021-09-29 17:02:16 -0300

    Listando repositorios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react"
 import { RepositoryItem } from "./RepositoryItem"
-const repository = { name: 'Exemplo', description: 'descrição', link: 'https://github.com/Renan1fps'}
 //https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList(){
   const [repositories, setRepositories] = useState([])
-  console.log(repositories)
-
+ 
   useEffect(()=>{
     const url ="https://api.github.com/orgs/rocketseat/repos"
     fetch(url).then(repos => repos.json()).then(data=>setRepositories(data))
@@ -15,10 +13,9 @@ export function RepositoryList(){
     <section>
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repo=>{
+          return <RepositoryItem key={repo.name}repository={repo}/>
+        })}
       </ul>
     </section>
   )

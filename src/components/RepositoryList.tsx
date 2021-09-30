fba1b63fc1da22b36a0ead2b7f429f9596408b8b
Renan <renan.sergipe68@gmail.com>
2021-09-30 15:29:49 -0300

    Tipando componentes

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import { useEffect, useState } from "react"
 import { RepositoryItem } from "./RepositoryItem"
 //https://api.github.com/orgs/rocketseat/repos
 
+interface Repository{
+  name: string,
+  description: string,
+  html_url: string
+}
+
 export function RepositoryList(){
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
  
   useEffect(()=>{
     const url ="https://api.github.com/orgs/rocketseat/repos"
@@ -14,7 +20,7 @@ export function RepositoryList(){
       <h1>Lista de repositórios</h1>
       <ul>
         {repositories.map(repo=>{
-          return <RepositoryItem key={repo.name}repository={repo}/>
+          return <RepositoryItem key={repo.name} repository={repo}/>
         })}
       </ul>
     </section>
